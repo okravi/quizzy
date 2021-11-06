57614a41174b985f1c726f57d5c43230ab789ad0
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-11-06 08:14:11 +0200

    progress update

--- app/src/main/java/com/example/quizzy/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizzy/QuizQuestionsActivity.kt
@@ -3,14 +3,23 @@ package com.example.quizzy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.example.quizzy.databinding.ActivityMainBinding
 import com.example.quizzy.databinding.ActivityQuizQuestionsBinding
 
 class QuizQuestionsActivity : AppCompatActivity() {
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_questions)
-    }*/
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
 
     private lateinit var binding: ActivityQuizQuestionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +28,31 @@ class QuizQuestionsActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        progressBar = binding.progressBar
+        tvProgress = binding.tvProgress
+        tvQuestion = binding.tvQuestion
+        ivImage = binding.ivImage
+
+        tvOptionOne = binding.tvOptionOne
+        tvOptionTwo = binding.tvOptionTwo
+        tvOptionThree = binding.tvOptionThree
+        tvOptionFour = binding.tvOptionFour
+
+
+
         val questionsList = Constants.getQuestions()
         Log.i("QuestionsList size is ", "${questionsList.size}")
+
+        for(i in questionsList){
+            Log.e("Questions", i.question)
+
+        }
+
+        var currentPosition = 1
+        val question: Question= questionsList[currentPosition - 1]
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "$currentPosition/${progressBar?.max}"
+        tvQuestion?.text = question.question
 
     }
 }
